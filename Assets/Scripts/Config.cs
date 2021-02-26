ef45343452086dced995770323f9ada56e02ec87
GaryTheKid <55671706+GaryTheKid@users.noreply.github.com>
2021-02-26 21:33:38 +0800

    2.26 Add Crosshair

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,4 +1,17 @@
-﻿public class Config
+﻿using UnityEngine;
+
+public class Config
 {
+    // IO
     public const float MOUSE_SENSITIVE = 2f;
+
+    // Interaction
+    public const float INTERACTION_RANGE = 8f;
+
+    // Color
+    public static Color INTERACTION_COLOR_DEFAULT = new Color(255f,255f,255f);
+    public static Color INTERACTION_COLOR_SELECTED = new Color(0f,200f,0f);
+
+    // Layer Index
+    public const int LAYER_INDEX_INTERACTABLE = 8;
 }
--- Assets/Scripts/Player Controller/PlayerInteractionController.cs
+++ Assets/Scripts/Player Controller/PlayerInteractionController.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteractionController : MonoBehaviour
 {
     public Transform playerCam;
+    public RawImage crossHair;
     public GameObject interactableObj;
 
     // Update is called once per fixed delta time
     void FixedUpdate()
     {
         // Only detect interactable objects in layer 8:"Interactable"
-        int layerMask = 1 << 8;
+        int layerMask = 1 << Config.LAYER_INDEX_INTERACTABLE;
         
         // Does the ray intersect any objects in layer 8
         RaycastHit hit;
-        if (Physics.Raycast(playerCam.position, playerCam.forward, out hit, 8f, layerMask))
+        if (Physics.Raycast(playerCam.position, playerCam.forward, out hit, Config.INTERACTION_RANGE, layerMask))
         {
             // Debug.DrawRay(playerCam.position, playerCam.forward * hit.distance, Color.yellow);
+
+            // Highlight Crosshair color
+            crossHair.color = Config.INTERACTION_COLOR_SELECTED;
 
             // select hit object
             interactableObj = hit.collider.gameObject;
@@ -25,16 +30,19 @@ public class PlayerInteractionController : MonoBehaviour
             // Highlight the outline & show notification text
             if (interactableObj != null)
             {
-                interactableObj.GetComponent<Outline>().OutlineColor = new Color(0f,200f,0f);
+                interactableObj.GetComponent<Outline>().OutlineColor = Config.INTERACTION_COLOR_SELECTED;
                 interactableObj.GetComponent<ShowNotification>().ShowText();
             }
         }
         else
         {
-            // Set outline back to default color & hide notification text
+            // Reset Crosshair color
+            crossHair.color = Config.INTERACTION_COLOR_DEFAULT;
+
+            // Reset outline color & hide notification text
             if (interactableObj != null)
             {
-                interactableObj.GetComponent<Outline>().OutlineColor = new Color(255f,255f,255f);
+                interactableObj.GetComponent<Outline>().OutlineColor = Config.INTERACTION_COLOR_DEFAULT;
                 interactableObj.GetComponent<ShowNotification>().HideText();
                 interactableObj = null;
             }
