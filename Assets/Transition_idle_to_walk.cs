1ed12558e2477fee35169a3e1a626e2adcb26b69
itserick25 <erickromero25@gmail.com>
2021-05-03 22:04:29 -0500

    Walking animation

---
+++ Assets/Transition_idle_to_walk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition_idle_to_walk : MonoBehaviour
+{
+    private Animator anim;
+    private float vert;
+
+    // Start is called before the first frame update
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vert = Input.GetAxis("Vertical");
+        anim.SetFloat("Walk", vert);
+        
+    }
+}
