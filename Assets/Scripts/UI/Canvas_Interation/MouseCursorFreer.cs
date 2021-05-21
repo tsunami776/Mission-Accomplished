96614996bb03a418bfcd425cde19d47746d0b80f
GaryTheKid <55671706+GaryTheKid@users.noreply.github.com>
2021-05-22 01:40:43 +0800

    5.21 Mouse Cursor Bug Fixed

--- Assets/Scripts/UI/Canvas_Interation/MouseCursorFreer.cs
+++ Assets/Scripts/UI/Canvas_Interation/MouseCursorFreer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueWindowController : MonoBehaviour
+public class MouseCursorFreer : MonoBehaviour
 {
     //[SerializeField] private GameObject closeButton;
 
